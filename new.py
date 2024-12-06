d522a3649ad6bbdb57055404981a3fa77d551129
Raizen22856 <assassincreedrevilations@gmail.com>
2024-12-06 18:05:19 +0700

    new.py

--- new.py
+++ new.py
@@ -2,4 +2,4 @@ Homework = 12
 Hours_Lost = 1.5
 Course = 'Python'
 Time_spent_on_one_task = Hours_Lost/Homework
-print('Курс:', Course,',','всего задач:',Homework,',','затрачено часов:',Hours_Lost,',','среднее время выполнения:',Time_spent_on_one_task,'часа.' )
+print('Курс: '+Course+', всего задач: '+str(Homework)+', затрачено часов: '+str(Hours_Lost)+', среднее время выполнения '+str(Time_spent_on_one_task)+' часа.' )
